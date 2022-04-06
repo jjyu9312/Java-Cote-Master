a766561949eac347c756b6992e06a01bef3e0978
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-04-07 00:09:05 +0900

    22/04/07 stack and queue 알고리즘

---
+++ src/stackandqueue/RemoveParentheses.java
@@ -0,0 +1,35 @@
+package stackandqueue;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class RemoveParentheses {
+
+    public String solution(String str) {
+        String answer = "";
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char x : str.toCharArray()) {
+            if (x == '(') stack.push(x);
+            else {
+                if (x == ')') {
+                    stack.pop();
+                }
+            }
+            if (stack.isEmpty()) answer += x;
+        }
+        answer = answer.replace(")", "");
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        RemoveParentheses niam = new RemoveParentheses(); // 매서드 static이면 객체 생성 필요 없음.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        System.out.println("=====================");
+        System.out.println(niam.solution(str));
+        System.out.println("=====================");
+    }
+}
