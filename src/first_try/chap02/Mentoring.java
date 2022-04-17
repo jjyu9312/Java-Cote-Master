e1fc92dee7e7028786386d111b2f393d4acaf7e6
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-04-17 23:20:25 +0900

    22/04/17 순서 알고리즘

--- src/first_try/chap02/Mentoring.java
+++ src/first_try/chap02/Mentoring.java
@@ -7,7 +7,20 @@ public class Mentoring {
     private int solution(int n, int m, int[][] arr) {
         int answer = 0;
 
-
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int cnt = 0;
+                for (int k = 0; k < m; k++) {
+                    int p1 = 0, p2 = 0;
+                    for (int s = 0; s < n; s++) {
+                        if (arr[k][s] == i) p1 = s;
+                        if (arr[k][s] == j) p2 = s;
+                    }
+                    if (p1 < p2) cnt++;
+                }
+                if (cnt == m) answer++;
+            }
+        }
         return answer;
     }
 
@@ -18,11 +31,12 @@ public class Mentoring {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[][] arr = new int[m][n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
+        System.out.println("=====================");
         System.out.println(niam.solution(n, m, arr));
         System.out.println("=====================");
     }
