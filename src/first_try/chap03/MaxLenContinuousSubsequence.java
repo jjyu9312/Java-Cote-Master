ffafb57ee59e4b3fc60500c8620613cc9d6c9a31
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-04-21 00:38:03 +0900

    22/04/21 \n Two pointers, Sliding window 알고리즘

---
+++ src/first_try/chap03/MaxLenContinuousSubsequence.java
@@ -0,0 +1,35 @@
+package first_try.chap03;
+
+import java.util.Scanner;
+
+public class MaxLenContinuousSubsequence {
+
+    private int solution(int n, int m, int[] arr) {
+        int answer = 0, cnt = 0, lt = 0;
+
+        for (int rt = 0; rt < n; rt++) {
+            if (arr[rt] == 0) cnt++;
+            while (cnt > m) {
+                if (arr[lt] == 0) cnt--; // 이미 1로 바뀐 값을 다시 0으로 바꿔주고 cnt는 감소
+                lt++;
+            }
+            answer = Math.max(answer, rt-lt+1);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        MaxLenContinuousSubsequence niam = new MaxLenContinuousSubsequence(); // 매서드 static이면 객체 생성 필요 없음.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("=====================");
+        System.out.println(niam.solution(n, m, arr));
+        System.out.println("=====================");
+    }
+}
