41fbc57dc7e59f555495fb3a678456e9bf372fe6
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-04-23 01:25:52 +0900

    22/04/23 \n Hash 알고리즘

--- src/first_try/chap04/AllSales.java
+++ src/first_try/chap04/AllSales.java
@@ -1,21 +1,27 @@
 package first_try.chap04;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class AllSales {
 
-    private int[] solution(int n, int k, int[] arr) {
-        int[] answer = new int[k];
+    private ArrayList<Integer> solution(int n, int k, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i <= n-k; i++) {
-            HashMap<Integer, Integer> map = new HashMap<>();
-            for (int j = i; j < k+i; j++) {
-                map.put(arr[j], map.getOrDefault(arr[j], 0) + 1);
-            }
-            answer[i] = map.size();
+        for (int i = 0; i < k-1; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
-        return answer;
+            int lt = 0;
+            for (int rt = k-1; rt < n; rt++) {
+                map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
+                answer.add(map.size());
+                map.put(arr[lt], map.get(arr[lt]) - 1);
+                if (map.get(arr[lt]) == 0) map.remove(arr[lt]);
+                lt++;
+            }
+            return answer;
     }
 
     public static void main(String[] args) {
@@ -23,7 +29,7 @@ public class AllSales {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc .nextInt();
-        int[] arr = new int [n];
+        int[] arr = new int[n];
         for (int i = 0; i < arr.length; i++) arr[i] = sc.nextInt();
         for (int x : niam.solution(n, k, arr)) System.out.print(x + " ");
     }
