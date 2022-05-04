b0ec7c91e948ea2a93bcf11703f392be9a3e649d
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-05-05 02:22:14 +0900

    22/05/05 \n Hash 알고리즘

--- src/first_try/chap05/KakaoDoll.java
+++ src/first_try/chap05/KakaoDoll.java
@@ -11,11 +11,11 @@ public class KakaoDoll {
         Stack<Integer> stack = new Stack<Integer>();
 
         for (int pos : moves) {
-            System.out.println("pos : " + pos);
+//            System.out.println("pos : " + pos);
             for (int i = 0; i < board.length; i++) { // 인형뽑기 내려감
                 if (board[i][pos - 1] != 0) {
                     int tmp = board[i][pos - 1];
-                    System.out.println("tmp : " + tmp);
+//                    System.out.println("tmp : " + tmp);
                     board[i][pos-1] = 0; // 인형 가져오고 없애기
                     if (!stack.isEmpty() && tmp == stack.peek()) { // peek : Stack의 top에 있는 item을 삭제하지않고 해당 item을 반환
                         answer += 2;
