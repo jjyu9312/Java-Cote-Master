740028a26a5f303926517131739038c2f96c8ecf
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-05-18 01:08:24 +0900

    22/05/18 \n Stack과 Queue 알고리즘

--- src/first_try/chap05/IronRod.java
+++ src/first_try/chap05/IronRod.java
@@ -1,11 +1,22 @@
 package first_try.chap05;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class IronRod {
 
     private int solution(String str) {
         int answer = 0;
+
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') stack.push('(');
+            else {
+                stack.pop(); // 레이저, 막대기 상관없이 빼기
+                if (str.charAt(i-1) == '(') answer += stack.size(); // 레이저이면 막대기 개수 증가
+                else answer++; // 막대기면 남는 하나 증가시킴
+            }
+        }
         return answer;
     }
 
