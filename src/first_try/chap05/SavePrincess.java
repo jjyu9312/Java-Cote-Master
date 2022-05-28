6b473d4ff6d8ae3055fadc1d1d45c3239b383ee0
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-05-29 00:09:03 +0900

    22/05/29 \n Stack과 Queue 알고리즘

---
+++ src/first_try/chap05/SavePrincess.java
@@ -0,0 +1,20 @@
+package first_try.chap05;
+
+import java.util.Scanner;
+
+public class SavePrincess {
+
+    public static void main(String[] args) {
+        SavePrincess niam = new SavePrincess(); // 매서드 static이면 객체 생성 필요 없음.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(niam.solution(a, b));
+    }
+
+    private int solution(int a, int b) {
+
+
+        return 0;
+    }
+}
