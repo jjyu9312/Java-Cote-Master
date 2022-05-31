10cfbfb9b888c04358b6c66a1bbe58561228c03e
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-06-01 00:39:33 +0900

    22/06/01 \n Stack과 Queue 알고리즘

--- src/first_try/chap05/SavePrincess.java
+++ src/first_try/chap05/SavePrincess.java
@@ -1,5 +1,7 @@
 package first_try.chap05;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class SavePrincess {
@@ -14,7 +16,18 @@ public class SavePrincess {
 
     private int solution(int a, int b) {
 
+        int answer = 0;
 
-        return 0;
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= a; i++) q.offer(i); // offer: queue에 값 추가
+
+        while (!q.isEmpty()) {
+            for (int i = 1; i < b; i++) q.offer(q.poll()); // poll: 꺼내고 값을 return 받음
+            q.poll();
+            if (q.size() == 1) answer = q.poll();
+        }
+
+        return answer;
     }
 }
