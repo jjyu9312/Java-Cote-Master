35179ee7180e8ff76272b442a9b174b476e6b20a
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-06-08 16:42:44 +0900

    22/06/08 \n Stack과 Queue 알고리즘

--- src/first_try/chap05/MakeEdu.java
+++ src/first_try/chap05/MakeEdu.java
@@ -3,5 +3,9 @@ package first_try.chap05;
 public class MakeEdu {
     public static void main(String[] args) {
         System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
