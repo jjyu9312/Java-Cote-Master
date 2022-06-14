5a3edc02ac968fb7c0a5f478879be8601b46e354
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-06-14 23:36:38 +0900

    22/06/14 \n Stack과 Queue 알고리즘

--- src/first_try/chap05/MakeEdu.java
+++ src/first_try/chap05/MakeEdu.java
@@ -8,9 +8,10 @@ public class MakeEdu {
     public static void main(String[] args) {
         MakeEdu niam = new MakeEdu(); // 매서드 static이면 객체 생성 필요 없음.
         Scanner sc = new Scanner(System.in);
-        String input1 = sc.nextLine();
-        String input2 = sc.nextLine();
+        String input1 = sc.next();
+        String input2 = sc.next();
         System.out.println(niam.solution(input1, input2));
+        System.out.println();
     }
 
     private String solution(String input1, String input2) {
@@ -20,9 +21,12 @@ public class MakeEdu {
         }
 
         for (char c : input2.toCharArray()) {
-            if (q.poll()) {
-                q.
+            if (q.contains(c)) {
+                if (c != q.poll()) return "NO"; // 맨 앞 자료를 꺼내면서 확인
             }
+        }
+        if (!q.isEmpty()) {
+            return "NO";
         }
         return "YES";
     }
